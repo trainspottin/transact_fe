69146ba97e88707f0a09383d826f0dfbebf639d2
Jimmy <trainspottin@gmail.com>
2017-01-16 01:37:26 +0800

    Update

--- main/server/controllers/application.js
+++ main/server/controllers/application.js
@@ -1,3 +1,4 @@
+'use strict';
 
 var noCache = function(res) {
     res.set('Cache-Control', 'no-cache, no-store, must-revalidate');
@@ -10,7 +11,33 @@ module.exports.index = function(req, res, next) {
     // 'X-FRAME-OPTIONS' indicate that wheather <frame>,<iframe>,<object> is loadable or not.
     res.set('X-FRAME-OPTIONS', 'DENY');
 
-    res.render('application', {head: {title:'title'}, body:{title: 'body title', description: 'description...'}});
+    var home_data = {
+        body : {
+            title: "Home Platform"
+        },
+        menu : {
+            "Home" : "/#/home",
+            "Admin" : {"User": "/#/amdin/user",
+                       "Group" : "/#/admin/group",
+                       "Resource" : "/#/admin/resource"
+                      },
+            "Bank" : {
+                "Bank List" : "/#/bank",
+                "Account" : "/#/account"
+            },
+            "Transaction" : { "Report" : "/#/transact/report",
+                              "Search" : "/#/transact/search",
+                              "Items" : "/#/transact/items"
+                            },
+            "Invest" : { "Gold" : "/#/invest/gold",
+                         "Stock" : "/#/invest/stock",
+                         "Currency" : "/#/invest/currency"
+                       }
+          },
+
+        }
+    };
+    res.render('application', home_data);
     //res.send("application.js");
     //next();
 };
